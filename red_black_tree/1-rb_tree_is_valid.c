1201568d09fb7553dbf549ba9a6f97a964195d54
Jonathan Boller <5533@holbertonstudents.com>
2023-11-13 22:53:30 -0600

    ???

--- red_black_tree/1-rb_tree_is_valid.c
+++ red_black_tree/1-rb_tree_is_valid.c
@@ -74,10 +74,9 @@ static int validate_colors(rb_tree_t *tree)
 
 	if (color == BLACK)
 	{
-		if (
-			(tree->left && (tree->left)->color == RED) ||
-			(tree->right && (tree->right)->color == RED)
-		)
+		if (tree->left && (tree->left)->color == RED)
+			return (0);
+		if (tree->right && (tree->right)->color == RED)
 			return (0);
 	}
 	return (
