c0be9477028df483c9abb1f77e358305ea4370d6
Jonathan Boller <5533@holbertonstudents.com>
2023-11-29 22:10:23 -0600

    Really? Really?

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -11,7 +11,15 @@
 */
 heap_t *heap_create(int (*data_cmp)(void *, void *))
 {
-	(void)data_cmp;
+	heap_t *new_heap;
 
-	return (NULL);
+	new_heap = malloc(sizeof(heap_t));
+	if (new_heap == NULL)
+		return (NULL);
+
+	new_heap->size = 0;
+	new_heap->data_cmp = data_cmp;
+	new_heap->root = NULL;
+
+	return (new_heap);
 }
