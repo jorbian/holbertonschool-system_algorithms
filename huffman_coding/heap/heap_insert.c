a4bdc3d365f47915dca13641bc733915e304cce4
Jonathan Boller <5533@holbertonstudents.com>
2023-12-05 13:52:09 -0600

    Meh?

--- huffman_coding/heap/heap_insert.c
+++ huffman_coding/heap/heap_insert.c
@@ -4,16 +4,54 @@
 #include "heap.h"
 
 /**
+ * place_higher - does exactly what it says on the tin
+ * @heap: pointer to the heap
+ * @current_node: pointer the current node
+ *
+ */
+static void place_higher(heap_t *heap, node_t *current_node)
+{
+	node_t *parent;
+
+	while (current_node->parent)
+	{
+		parent = current_node->parent;
+
+		if (heap->data_cmp(parent->data, current_node->data) > 0)
+		{
+			parent->data = current_node->data;
+			current_node->data = parent->data;
+			current_node = parent;
+		}
+		else
+			break;
+	}
+}
+
+/**
  * heap_insert - inserts a value in a Min Binary Heap
  * @heap: pointer to the heap in which the node has to be inserted
  * @data: pointer containing data to store in new node
  *
  * Return: pointer to new node containing data, or NULL if it fails
 */
-node_t *heap_insert(heap_t *heap, void *data)
+node_t *heap_insert(heap_t *heap, void *new_data)
 {
-	(void)heap;
-	(void)data;
+	node_t *new_node;
 
-	return (NULL);
+	if (!heap || !new_data)
+		return (NULL);
+
+	new_node = binary_tree_node(heap->root, new_data);
+	if (!new_node)
+		return (NULL);
+
+	if (!heap->root)
+		heap->root = new_node;
+	else
+		place_higher(heap, new_node);
+
+	heap->size++;
+
+	return (new_node);
 }
