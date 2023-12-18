788b5deb77ae42436e6cc9204cfb4073d557d6ce
Jonathan Boller <5533@holbertonstudents.com>
2023-12-17 20:33:19 -0600

    First real attempt at second task

--- nary_trees/1-nary_tree_delete.c
+++ nary_trees/1-nary_tree_delete.c
@@ -3,8 +3,25 @@
 /**
  * nary_tree_delete - deallocates an entire N-ary tree
  * @tree: pointer to root node of nary tree
+ *
 */
 void nary_tree_delete(nary_tree_t *tree)
 {
-	(void)tree;
+	nary_tree_t *node_pointer;
+
+	if (tree == NULL)
+		return;
+
+	while (tree)
+	{
+		node_pointer = tree;
+
+		tree = tree->next;
+
+		nary_tree_delete(
+			node_pointer->children
+		);
+		free(node_pointer->content);
+		free(node_pointer);
+	}
 }
